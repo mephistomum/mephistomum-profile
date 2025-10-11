4d87e9858cfe52000c417bf9b5867c3ce10e0704
Nurul Izzah <mephistomum@gmail.com>
2025-10-11 23:00:40 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./assets/js/**/*.js"],
+  theme: {
+    extend: {    
+      colors: {
+        beige: "#C9B2A0",
+        brown: "#47281F",
+        nav: "#956C4F"
+      },
+    },
+    
+  },
+  plugins: [],
+}
